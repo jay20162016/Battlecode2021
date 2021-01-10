fcb669f5c1a8a17eaf94c63a0c8eaf1836191283
Jay Jayjay <jayjayjay@example.fios-router.home>
2021-01-09 19:08:26 -0500

    update my bot;

--- src/initialjaybot/RobotPlayer.java
+++ src/initialjaybot/RobotPlayer.java
@@ -11,18 +11,22 @@ public strictfp class RobotPlayer {
 
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
+      if (rc.getRoundNum() > 1500) {
+  rc.resign();
+}
+
 
         // This is the RobotController object. You use it to perform actions from this robot,
         // and to get information on its current status.
         RobotPlayer.rc = rc;
 
-        turnCount = 0;
+        RobotPlayer.turnCount = 0;
 
 
         Team me = rc.getTeam();
         int sensorRadius = rc.getType().sensorRadiusSquared;
-        RobotInfo[] attackable = rc.senseNearbyRobots(sensorRadius, me);
-        for (RobotInfo robot : attackable) {
+        RobotInfo[] allies = rc.senseNearbyRobots(sensorRadius, me);
+        for (RobotInfo robot : allies) {
             if (robot.type == RobotType.ENLIGHTENMENT_CENTER) {
                   RobotPlayer.enlightenmentCenter = robot.getLocation();
                   break;
@@ -30,7 +34,7 @@ public strictfp class RobotPlayer {
         }
 
         while (true) {
-            turnCount += 1;
+            RobotPlayer.turnCount += 1;
             // Try/catch blocks stop unhandled exceptions, which cause your robot to freeze
             try {
                 // Here, we've separated the controls into a different method for each RobotType.
@@ -53,14 +57,13 @@ public strictfp class RobotPlayer {
     }
 
     static void runEnlightenmentCenter() throws GameActionException {
-      System.out.println(rc.getInfluence());
-      System.out.println(rc.getConviction());
-        RobotType toBuild = Game.randomSpawnableRobotType(0.2, 0.4, 0.4);
-        int influence = 1;
-        if (toBuild == RobotType.POLITICIAN) {
-          influence = 200;
-        }
-        else if (toBuild == RobotType.MUCKRAKER) {
+      // System.out.println("initialjaybot - infl, conv");
+      // System.out.println(rc.getInfluence());
+      // System.out.println(rc.getConviction());
+        // RobotType toBuild = Game.randomSpawnableRobotType(0.2, 0.4, 0.4);
+        RobotType toBuild = Game.randomSpawnableRobotType(0.2, 0.2, 0.6);
+        int influence = rc.getInfluence() - 150;
+        if (toBuild == RobotType.MUCKRAKER && Math.random() < 400/(rc.getRoundNum()+1)) {
           influence = 1;
         }
         for (Direction dir : Game.directions) {
@@ -72,23 +75,34 @@ public strictfp class RobotPlayer {
 
     static void runPolitician() throws GameActionException {
         Team enemy = rc.getTeam().opponent();
+        int sensorRadius = rc.getType().sensorRadiusSquared;
         int actionRadius = rc.getType().actionRadiusSquared;
-        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
-        if (rc.canEmpower(actionRadius)) {
-            for (RobotInfo robot : attackable) {
-                // if (robot.type == RobotType.ENLIGHTENMENT_CENTER) {
+        if (rc.canEmpower(actionRadius) && rc.getCooldownTurns() == 0) {
+            for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+                if ((robot.type == RobotType.ENLIGHTENMENT_CENTER || Math.random() < 0.1) &&
+                rc.getLocation().isWithinDistanceSquared(robot.location, actionRadius)) {
                       rc.empower(actionRadius);
+                      // System.out.println("initialjaybot - BIG BOOM KAMIKAZE!!!");
                       return;
-                // }
+                }
+                else {
+                      Game.tryMove(rc, rc.getLocation().directionTo(robot.getLocation()));
+                }
+            }
+            for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, Team.NEUTRAL)) {
+                rc.empower(actionRadius);
+                // System.out.println("initialjaybot - BIG BOOM KAMIKAZE!!!");
+                return;
             }
         }
+        for (RobotInfo robot : rc.senseNearbyRobots(sensorRadius, Team.NEUTRAL)) {
+            Game.tryMove(rc, rc.getLocation().directionTo(robot.getLocation()));
+        }
+        for (RobotInfo robot : rc.senseNearbyRobots(sensorRadius, enemy)) {
+          Game.tryMove(rc, rc.getLocation().directionTo(robot.getLocation()));
+        }
 
-        if (Math.random() < 0.8) {
-          Game.tryMove(rc, Game.randomDirection());
-        }
-        else {
-          Game.tryMove(rc, enlightenmentCenter.directionTo(rc.getLocation()));
-        }
+        Game.tryMove(rc, Game.randomDirection());
     }
 
     static void runSlanderer() throws GameActionException {
@@ -97,7 +111,8 @@ public strictfp class RobotPlayer {
         for (RobotInfo robot : rc.senseNearbyRobots(sensorRadius, enemy)) {
             if (robot.type == RobotType.MUCKRAKER) {
                 // AAAAARRRGGHHH! it's a muckraker!
-                if (Game.tryMove(rc, robot.location.directionTo(rc.getLocation()))) return;
+                if (Game.tryMove(rc, robot.getLocation().directionTo(rc.getLocation()))) return;
+                // System.out.println("initialjaybot - A MUCKRAKER ARCHER !!!");
             }
         }
         Game.tryMove(rc, Game.randomDirection());
@@ -105,31 +120,37 @@ public strictfp class RobotPlayer {
 
     static void runMuckraker() throws GameActionException {
         Team enemy = rc.getTeam().opponent();
+        int sensorRadius = rc.getType().sensorRadiusSquared;
         int actionRadius = rc.getType().actionRadiusSquared;
         for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
-            if (robot.type.canBeExposed()) {
+            if (robot.type.canBeExposed() && rc.canExpose(robot.location) &&
+            rc.getLocation().isWithinDistanceSquared(robot.location, actionRadius)) {
                 // It's a slanderer... go get them!
-                if (rc.canExpose(robot.location)) {
-                    rc.expose(robot.location);
-                    return;
-                }
+                rc.expose(robot.location);
+                // System.out.println("initialjaybot - e x p o s e d and shot");
+                return;
+            }
+            else {
+                Game.tryMove(rc, rc.getLocation().directionTo(robot.getLocation()));
             }
         }
-        if (rc.senseNearbyRobots(actionRadius, enemy).length > 0) {
-          Game.tryMove(rc, Game.randomDirection());
-          return;
+        for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+          Game.tryMove(rc, rc.getLocation().directionTo(robot.getLocation()));
         }
 
         MapLocation loc = rc.getLocation(); //lattice
         for (Direction dir : Game.directions) {
           for (Direction dir2 : Game.directions) {
             Direction d3 = Game.randomDirection();
-            if (!(((loc.add(d3).x % 3) == 1) ^ ((loc.add(d3).y % 3) == 1))) {
+            if (!(
+                  ((loc.add(d3).x % 3) == 1) ^ ((loc.add(d3).y % 3) == 1)
+                  )) {
+            // if (!((loc.add(d3).x+loc.add(d3).x) % 2) == 1) {
               Game.tryMove(rc, d3);
               return;
             }
           }
         }
-        // Game.tryMove(rc, Game.randomDirection());
+        Game.tryMove(rc, Game.randomDirection());
     }
 }
